ec3aa2ade44716ce57e0923c727d95294af349db
Taehun Kim <kimx2815@umn.edu>
2013-10-21 23:19:52 -0500

    Update SimpleUserUserItemScorer.java

--- CSci_5980/uu-assignment/src/main/java/edu/umn/cs/recsys/uu/SimpleUserUserItemScorer.java
+++ CSci_5980/uu-assignment/src/main/java/edu/umn/cs/recsys/uu/SimpleUserUserItemScorer.java
@@ -21,7 +21,9 @@ import javax.inject.Inject;
 public class SimpleUserUserItemScorer extends AbstractItemScorer {
     private final UserEventDAO userDao;
     private final ItemEventDAO itemDao;
-
+    
+    private static final int NEIGHBOR = 30 ;
+    
     @Inject
     public SimpleUserUserItemScorer(UserEventDAO udao, ItemEventDAO idao) {
         userDao = udao;
@@ -31,12 +33,18 @@ public class SimpleUserUserItemScorer extends AbstractItemScorer {
     @Override
     public void score(long user, @Nonnull MutableSparseVector scores) {
         SparseVector userVector = getUserRatingVector(user);
-
-        // TODO Score items for this user using user-user collaborative filtering
+        double meanUser = userVector.mean();
+        double score ;
 
         // This is the loop structure to iterate over items to score
         for (VectorEntry e: scores.fast(VectorEntry.State.EITHER)) {
+            long itemNo = e.getKey();
+            List<Neighbor> neighbors = findNearNeighbor(itemNo, user, userVector, NEIGHBOR);
+            score = score(meanUser, itemNo, neighbors);
+            
+            scores.set(e,score);
 
+            
         }
     }
 
@@ -52,4 +60,50 @@ public class SimpleUserUserItemScorer extends AbstractItemScorer {
         }
         return RatingVectorUserHistorySummarizer.makeRatingVector(history);
     }
+    
+    private class Neighbor {
+        public final long userNo;
+        public final SparseVector userRate;
+        public final double simscore; //similarity value
+    }
+    
+    /**
+     * Get User similarity mentioned in lecture 
+     * @param user1 userNo 1
+     * @param user2 userNo 2
+     * @return cosine similarity
+     */
+     private double computeUserSimilarity(long user1, long user2) {
+         CosineVectorSimilarity cosineVector = new CosineVectorSimilarity();
+         SparseVector sv1 = getUserRatingVector(user1);
+         SparseVector sv2 = getUserRatingVector(user2);
+         
+         MutableSparseVector msv1 = sv1.mutableCopy();
+         MutableSparseVector msv2 = sv2.mutableCopy();
+         msv1.add(-1*sv1.mean());
+         msv2.add(-1*sv2.mean());
+
+         return cosineVector.similarity(sv1, sv2);
+     }
+     
+     //pu,i=μu+∑v∈N(u;i)s(u,v)(rv,i−μv)∑v∈N(u;i)|s(u,v)|
+     
+    /**
+     * ∑v∈N(u;i)s(u,v)(rv,i−μv)
+     * @param neighbors
+     * @param itemNo
+     * @return result   
+     */
+    private double getNumerator(List<Neighbor> neighbors, long itemNo )
+    {
+            double result = 0;
+            for(Neighbor n : neighbors) {
+                    double rating = n.userRating.get(itemNo, 0);
+                    result = result + n.similarity * (rating - n.getMean());
+            }
+            return result;
+    }
+    
+    
+     
 }
